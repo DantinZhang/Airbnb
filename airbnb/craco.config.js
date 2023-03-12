bca9894f3bc323829f076cc547016dc54576fb03
zzy <1246486788@qq.com>
2023-03-12 21:45:19 +0800

    项目的目录搭建，目录的别名，less的安装

---
+++ airbnb/craco.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const CracoLessPlugin = require('craco-less');
+
+module.exports = {
+    //下面的配置和脚手架webpack合并
+    webpack: {
+        alias: {
+            "@": path.resolve(__dirname, 'src'),
+            'components': path.resolve(__dirname, 'src/components'),
+            'store':path.resolve(__dirname,'src/store'),
+        }
+    },
+
+    //配置less
+    plugins: [
+        {
+            plugin: CracoLessPlugin
+        }
+    ]
+}
