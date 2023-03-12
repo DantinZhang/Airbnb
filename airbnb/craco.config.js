d181b320404b21edfaafb61ea0ad5913618a43f3
zzy <1246486788@qq.com>
2023-03-12 22:16:45 +0800

    CSS的基本样式设置

--- airbnb/craco.config.js
+++ airbnb/craco.config.js
@@ -8,6 +8,7 @@ module.exports = {
             "@": path.resolve(__dirname, 'src'),
             'components': path.resolve(__dirname, 'src/components'),
             'store':path.resolve(__dirname,'src/store'),
+            'assets':path.resolve(__dirname, 'src/assets'),
         }
     },
 
