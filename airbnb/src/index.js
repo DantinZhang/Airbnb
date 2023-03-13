4592f623cad0d8bcdabbd4cf8fec646c469ab377
zzy <1246486788@qq.com>
2023-03-13 09:18:01 +0800

    我的提交都去哪里了！！

--- airbnb/src/index.js
+++ airbnb/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 import 'normalize.css';  //先引入模板
@@ -7,5 +8,7 @@ import '@/assets/css/reset.css' //引入自定义样式
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
