8a6a66ad2976f5434da63803b02a8d29893060df
zzy <1246486788@qq.com>
2023-03-13 15:39:26 +0800

    测试axios发送网络请求

--- airbnb/src/views/Home/index.jsx
+++ airbnb/src/views/Home/index.jsx
@@ -1,6 +1,14 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect } from 'react';
+import hyRequest from '@/api';
 
 const Home = memo(() => {
+
+  useEffect(() => {
+    hyRequest.get({url:'/home/highscore'}).then(res => {
+      console.log(res);
+    })
+  },[])
+
   return (
     <div>
       <h1>Home</h1>
