6f002de944a43d6478bf4d35e8a6bb95b80cf2a4
zzy <1246486788@qq.com>
2023-03-13 13:45:39 +0800

    路由懒加载的配置

--- airbnb/src/index.js
+++ airbnb/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
@@ -9,6 +9,8 @@ import '@/assets/css/reset.css' //引入自定义样式
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter>
-    <App />
+    <Suspense fallback={<h3>路由加载中……请稍后……</h3>}>
+      <App />
+    </Suspense>
   </HashRouter>
 );
--- airbnb/src/router/index.js
+++ airbnb/src/router/index.js
@@ -1,8 +1,12 @@
 import React from "react";
-import Home from "@/views/Home";
-import Detail from "@/views/Detail";
-import Entire from "@/views/Entire";
+// import Home from "@/views/Home";
+// import Detail from "@/views/Detail";
+// import Entire from "@/views/Entire";
 import { Navigate } from "react-router-dom";
+
+const Home = React.lazy(() => import('@/views/Home'));
+const Detail = React.lazy(() => import('@/views/Detail'));
+const Entire = React.lazy(() => import('@/views/Entire'));
 
 let routes = [
     {
