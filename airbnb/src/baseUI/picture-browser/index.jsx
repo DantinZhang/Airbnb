4d3895906a5d2ebcf875954bd2509d6cd783b124
zzy <1246486788@qq.com>
2023-03-21 22:31:24 +0800

    滚动条的消失和隐藏

--- airbnb/src/baseUI/picture-browser/index.jsx
+++ airbnb/src/baseUI/picture-browser/index.jsx
@@ -1,13 +1,22 @@
 import PropTypes from 'prop-types'
-import React, { memo } from 'react'
+import React, { memo, useEffect } from 'react'
 import BrowserWrapper from './style'
 
 const PictureBrowser = memo((props) => {
-  return (
-    <BrowserWrapper>
-        图片浏览器
-    </BrowserWrapper>
-  )
+    useEffect(() => {
+        //图片浏览器渲染完成后，隐藏滚动条
+        document.body.style.overflow = 'hidden';
+        return () => {
+            //图片浏览器关闭后，显示滚动条
+            document.body.style.overflow = 'auto';
+        }
+    }, [])
+
+    return (
+        <BrowserWrapper>
+            图片浏览器
+        </BrowserWrapper>
+    )
 })
 
 PictureBrowser.propTypes = {}
